aea51586795ca6db0b7cec65fb1ec9cad399923c
Nobkun16 <johnphilipbaylon17@gmail.com>
2025-04-04 16:52:57 +0800

    Initial commit

---
+++ login/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("verification", views.generate_otp, name="otp"),
+    path("admin_panel", views.admin_panel, name="admin_panel")
+
+]
